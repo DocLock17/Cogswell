6d008e64b465c2e85f36f4059321daf470cd0d06
doclock17 <michael.c.locker@gmail.com>
2021-09-25 13:26:44 -0500

    Final edits on Forge

--- IntentClassifierForge.py
+++ IntentClassifierForge.py
@@ -1,7 +1,6 @@
 #!/bin/bash/python3
-# This is some clean up code that should ensure the script runs virtually anywhere
 
-
+# Scripted installs for Colab use
 try:
   import simpleaudio as sa
 except:
@@ -41,14 +40,14 @@ try:
 except:
   nltk.download('averaged_perceptron_tagger')
 
-try:
-  test_stop = stopwords.words("english")
-except:
-  nltk.download("stopwords")
+# try:
+#   test_stop = stopwords.words("english")
+# except:
+#   nltk.download("stopwords")
 
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 from nltk.util import ngrams
 
 try:
@@ -67,7 +66,6 @@ import pickle
 import json
 import random
 import re
-# import pyaudio
 
 def get_wordnet_pos(treebank_tag):
     if treebank_tag.startswith('J'):
@@ -110,7 +108,21 @@ Example:
     return ' '.join(t for t in txt)
 
 def n_gram_split(text, n=2, string_mode=True, keep_original=True):
-  """ Takes in a string and returns a list of n_grams."""
+  """Takes in a string and returns a list of n_grams or a modified string 
+  containing of sequenced n_grams. With or without the original tokens.
+  
+    text: (string), 
+    n: (number of tokens per gram), 
+    string_mode: (True=returns string, False=returns tokens),
+    keep_original: (True=return contains original tokens, False=return is contains only n_grams)
+        
+
+    Example:
+        ---------
+        >>> text = n_gram_split("I really really really like data science.")
+        >>> print(text)
+        I I_really really really_like like like_data data data_science science.
+    """
   the_grams = ngrams(text.split(), n)
   gram_list = []
   for each in the_grams:
@@ -179,18 +191,11 @@ def clean_up_sentence(text):
     # Strip off spaces
     text = text.strip(' ')
 
-    ### Probably should split the logic here ###
-
     # Tokenize the text (split them into lists of words)
     text_tokens = nltk.word_tokenize(text)
-    # text_tokens = text.split()
-    # sw = stopwords.words("english")
-    # text_tagged = [t for t in nltk.pos_tag(text_tokens) if t[0] not in sw]
     text_tagged = [t for t in nltk.pos_tag(text_tokens)]
     final_text = ' '.join(lemmatizer.lemmatize(w[0], pos=get_wordnet_pos(w[1])) for w in text_tagged)
     remove_repeated_words(final_text)
-    # final_text = [lemmatizer.lemmatize(word.lower()) for word in text_tokens]
-
     return final_text
 
 import nltk
@@ -219,63 +224,59 @@ intents = json.loads(data_file)
 for intent in intents['intents']:
     for pattern in intent['patterns']:
 
-        # take each word and tokenize it
+        # Tokenize each intent pattern
         w = clean_up_sentence(pattern)
+
+        # Get n_gram tokens
         w = n_gram_split(w, n=2, string_mode=False, keep_original=True)
-        # w = nltk.word_tokenize(pattern)
+        
+        # Add all grams to words list
         words.extend(w)
-        # adding documents
+        
+        # Add intents and gram patterns to documents list
         documents.append((w, intent['tag']))
 
-        # adding classes to our class list
+        # Generate class list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+# Clean up
 words = sorted(list(set(words)))
-
 classes = sorted(list(set(classes)))
 
+# Output stats
 print (len(documents), "documents")
 
 print (len(classes), "classes", classes)
 
 print (len(words), "unique lemmatized words", words)
 
-
+# Pickle words and classes for use in production
 pickle.dump(words,open('listWords.pkl','wb'))
 pickle.dump(classes,open('listClasses.pkl','wb'))
 
-# initializing training data
+# Generate training data
 training = []
 output_empty = [0] * len(classes)
+
+# Generate 1 hot bag of words arrays for each intent pattern
 for doc in documents:
-    # initializing bag of words
     bag = []
-    # list of tokenized words for the pattern
     pattern_words = doc[0]
-    # lemmatize each word - create base word, in attempt to represent related words
-    # pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # create our bag of words array with 1, if word match found in current pattern
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
-
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
-
     training.append([bag, output_row])
-# shuffle our features and turn into np.array
+
+# Shuffle and split our dataset
 random.shuffle(training)
 training = np.array(training)
-# create train and test lists. X - patterns, Y - intents
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 print("Training data created")
 
-
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-# equal to number of intents to predict output intent with softmax
+# Build a basic sequential model for categorization
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -283,12 +284,17 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+# Set optimizer (SGD with Nesrov)
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+
+# Compile model with categorical loss
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-#fitting and saving the model
+# Train model for 200 epochs
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+
+# Save model for production
 model.save('language_model0.h5', hist)
 
-print("model created")
+# Signal completion
+print("Model Created")
